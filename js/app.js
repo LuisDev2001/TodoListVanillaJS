b80e357e1bab00b83e7231ff6a208fd5b4347a72
Luis Quispe <luisqrja@gmail.com>
2020-09-18 21:36:58 -0500

    Fix functionallity for delete task

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function load() {
   const $form = document.getElementById("js_form-task");
   const $containerTask = document.getElementById("js_container-task");
+
   function templateTask(taskValue, $container) {
     return ($container.innerHTML += `
       <li class="print-task-item">
@@ -9,53 +10,36 @@ function load() {
           ${taskValue}
         </span>
         <section class="buttons-actions">
-          <div id="js_delete">
-            <i class="far fa-trash-alt"></i>
-          </div>
-          <div id="js_edit">
-            <i class="fas fa-pencil-alt"></i>
-          </div>
+          <div id="js_delete"></div>
+          <div id="js_edit"></div>
         </section>
       </li>
     `);
   }
 
-  function eventDeleteTask($btnDelete) {
-    $btnDelete.forEach((element) => {
-      element.addEventListener("click", (event) => {
-        event.preventDefault();
-        console.log("Click Eliminar");
-        const itemLiskTask = document.querySelector(".print-task-item");
-        $containerTask.removeChild(itemLiskTask);
-      });
-    });
-  }
-
-  function eventEditTask($btnEdit) {
-    $btnEdit.forEach((element) => {
-      element.addEventListener("click", (event) => {
-        event.preventDefault();
-        console.log("Click edit");
-      });
-    });
+  function deleteTask(event) {
+    event.preventDefault();
+    const btnContent = event.target.parentNode;
+    const item = btnContent.parentNode;
+    const ul = item.parentNode;
+    ul.removeChild(item);
   }
 
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
     const data = new FormData($form);
     const inputTaskValue = data.get("text_Task");
+    const $messageEmptyTask = document.getElementById("js_message-empty-task");
+    $messageEmptyTask.classList.remove("active");
     document.getElementById("input-task").value = "";
 
     templateTask(inputTaskValue, $containerTask);
 
+    //Delete a task
     const $btnDelete = document.querySelectorAll("#js_delete");
-    eventDeleteTask($btnDelete);
-
-    const $editButton = document.querySelectorAll("#js_edit");
-    eventEditTask($editButton);
-
-    const $messageEmptyTask = document.getElementById("js_message-empty-task");
-    $messageEmptyTask.classList.remove("active");
+    $btnDelete.forEach((element) => {
+      element.addEventListener("click", deleteTask);
+    });
   });
 }
 
