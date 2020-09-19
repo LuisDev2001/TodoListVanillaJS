4f53f582422093c4f9968a84f83d82f2ddc1b742
Luis Quispe <luisqrja@gmail.com>
2020-09-19 10:32:33 -0500

    Avance de functionallity Update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function load() {
   const $form = document.getElementById("js_form-task");
   const $containerTask = document.getElementById("js_container-task");
+  const $btnSubmit = document.querySelector("#js_btn-send");
 
   function templateTask(taskValue, $container) {
     return ($container.innerHTML += `
@@ -18,13 +19,24 @@ function load() {
   }
 
   function deleteTask(event) {
-    event.preventDefault();
     const btnContent = event.target.parentNode;
     const item = btnContent.parentNode;
     const ul = item.parentNode;
     ul.removeChild(item);
   }
 
+  function editTask(event) {
+    const btnContent = event.target.parentNode;
+    const item = btnContent.parentNode;
+    const text = item.querySelector("span").textContent.trim();
+    $btnSubmit.classList.remove("btn-send");
+    $btnSubmit.classList.add("btn-update");
+    $btnSubmit.innerHTML = `<i class="fas fa-sync-alt"></i> Update`;
+    item.classList.add("active");
+    document.getElementById("input-task").value = text;
+  }
+
+  //Submit a newTask
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
     const data = new FormData($form);
@@ -32,13 +44,18 @@ function load() {
     const $messageEmptyTask = document.getElementById("js_message-empty-task");
     $messageEmptyTask.classList.remove("active");
     document.getElementById("input-task").value = "";
-
     templateTask(inputTaskValue, $containerTask);
 
     //Delete a task
     const $btnDelete = document.querySelectorAll("#js_delete");
     $btnDelete.forEach((element) => {
       element.addEventListener("click", deleteTask);
+    });
+
+    //Edit task
+    const $btnEdit = document.querySelectorAll("#js_edit");
+    $btnEdit.forEach((element) => {
+      element.addEventListener("click", editTask);
     });
   });
 }
