a047207472f35b252a1b4a84c478776be243fbc6
LuisDev2001 <cuarto87@hotmail.com>
2020-11-19 22:45:04 -0500

    Add a funcionallity for put line through in a task ended :)

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@
   const $form = document.getElementById("js_form-task");
   const $containerTask = document.getElementById("js_container-task");
 
+  /**
+   * Function to print a new task
+   * @param {*} taskValue
+   * @param {*} $container
+   */
   function templateTask(taskValue, $container) {
     return ($container.innerHTML += `
       <li class="print-task-item">
@@ -19,6 +24,10 @@
     `);
   }
 
+  /**
+   * Action for delete a task
+   * @param {*} $btnDelete
+   */
   function deleteTask($btnDelete) {
     const btnContent = $btnDelete.target.parentNode;
     const li = btnContent.parentNode;
@@ -26,6 +35,10 @@
     ul.removeChild(li);
   }
 
+  /**
+   * Action for edit and Update Task
+   * @param {*} $btnEdit
+   */
   function editAndUpdateTask($btnEdit) {
     const btnContent = $btnEdit.target.parentNode;
     const $btnEditUpdate = btnContent.querySelector(".js_edit_update");
@@ -44,6 +57,16 @@
     $btnEditUpdate.classList.toggle("active");
   }
 
+  /**
+   * Event for put line-through in a text
+   * @param {*} $checbox
+   */
+  function taskEnded($checbox) {
+    const contetnCheckbox = $checbox.target.parentNode;
+    const task = contetnCheckbox.querySelector(".task");
+    task.classList.add("ended");
+  }
+
   $containerTask.addEventListener("click", (event) => {
     /**
      * Event for delete, edit and update buttons a task
@@ -57,13 +80,18 @@
     ) {
       console.log("Editar and update");
       editAndUpdateTask(event);
+    } else if (event.target.type === "checkbox") {
+      console.log("Task ended");
+      taskEnded(event);
     } else {
       return false;
     }
   });
+
   //Submit a newTask
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
+    console.log(event);
     const data = new FormData($form);
     const inputTaskValue = data.get("text_Task");
     const $messageEmptyTask = document.getElementById("js_message-empty-task");
