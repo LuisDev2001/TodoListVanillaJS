06ce1ed8b500a5a426d1ea13ff2a418188afc083
LuisDev2001 <cuarto87@hotmail.com>
2020-11-19 22:34:23 -0500

    COMPLETE CRUD FOR JS FRONTEND CHALLENGE

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-function load() {
+(function load() {
+  console.log("JS CONNECT! :)");
   const $form = document.getElementById("js_form-task");
   const $containerTask = document.getElementById("js_container-task");
-  const $btnSubmit = document.querySelector("#js_btn-send");
 
   function templateTask(taskValue, $container) {
     return ($container.innerHTML += `
@@ -9,46 +9,58 @@ function load() {
         <span>
           <input type="checkbox" />
           <span class="task">${taskValue}</span>
-          <input type="text" id="js_input-edit" />
+          <input type="text" class="input-edit" />
         </span>
         <section class="buttons-actions">
-          <div id="js_delete"></div>
-          <div id="js_edit"></div>
-          <div id="js_update" style="display: none;"></div>
+          <div class="js_delete"></div>
+          <div class="js_edit_update"></div>
         </section>
       </li>
     `);
   }
 
-  function deleteTask(event) {
-    const btnContent = event.target.parentNode;
-    const item = btnContent.parentNode;
-    const ul = item.parentNode;
-    ul.removeChild(item);
+  function deleteTask($btnDelete) {
+    const btnContent = $btnDelete.target.parentNode;
+    const li = btnContent.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
   }
 
-  function editTask(event) {
-    const btnContent = event.target.parentNode;
-    const item = btnContent.parentNode;
-    let text = item.querySelector(".task").textContent.trim();
-    const $btnEdit = document.querySelector("#js_edit");
-    const $btnUpdate = document.querySelector("#js_update");
-    const editInput = document.querySelector("#js_input-edit");
-    $btnEdit.style.display = "none";
-    $btnUpdate.style.display = "flex";
-
-    item.classList.add("active-edit__mode");
-    editInput.setAttribute("value", text);
-
-    $btnUpdate.addEventListener("click", () => {
-      console.log("Click");
-      item.querySelector(".task").textContent = editInput.value;
-      item.classList.remove("active-edit__mode");
-      $btnEdit.style.display = "flex";
-      $btnUpdate.style.display = "none";
-    });
+  function editAndUpdateTask($btnEdit) {
+    const btnContent = $btnEdit.target.parentNode;
+    const $btnEditUpdate = btnContent.querySelector(".js_edit_update");
+    const li = btnContent.parentNode;
+    const task = li.querySelector(".task");
+    //This is the old value
+    const oldValue = task.innerText.trim();
+    const inputEdit = li.querySelector(".input-edit");
+    //Set attribute for save value
+    inputEdit.setAttribute("value", oldValue);
+    //Get new value for the input
+    const newValue = inputEdit.value;
+    //Replace new task in the span element
+    task.innerText = newValue;
+    li.classList.toggle("active-edit__mode");
+    $btnEditUpdate.classList.toggle("active");
   }
 
+  $containerTask.addEventListener("click", (event) => {
+    /**
+     * Event for delete, edit and update buttons a task
+     */
+    if (event.target.className === "js_delete") {
+      console.log("Eliminar");
+      deleteTask(event);
+    } else if (
+      event.target.className === "js_edit_update" ||
+      event.target.className === "js_edit_update active"
+    ) {
+      console.log("Editar and update");
+      editAndUpdateTask(event);
+    } else {
+      return false;
+    }
+  });
   //Submit a newTask
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -57,20 +69,9 @@ function load() {
     const $messageEmptyTask = document.getElementById("js_message-empty-task");
     $messageEmptyTask.classList.remove("active");
     document.getElementById("input-task").value = "";
+    /**
+     * When the form submit information print task in the list
+     */
     templateTask(inputTaskValue, $containerTask);
-
-    //Delete a task
-    const $btnDelete = document.querySelectorAll("#js_delete");
-    $btnDelete.forEach((element) => {
-      element.addEventListener("click", deleteTask);
-    });
-
-    //Edit task
-    const $btnEdit = document.querySelectorAll("#js_edit");
-    $btnEdit.forEach((element) => {
-      element.addEventListener("click", editTask);
-    });
   });
-}
-
-load();
+})();
